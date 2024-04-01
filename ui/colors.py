3c45d4fb827ea26a6fa280929bb56ab91263bdfa
ImGdevel <imdlsrks.mc@gmail.com>
2024-04-01 23:12:11 +0900

    color sheet

---
+++ ui/colors.py
@@ -0,0 +1,17 @@
+# colors.py
+
+class Colors:
+    #Title Bar Colors
+    titleBarBackgroundColor = '#1a1a1a'
+    cancelBtnColor = '#c33d3d'
+
+    #TextColorSheet
+    textColor01 = '#ffffff'
+    baseColor01 = '#212121'
+
+    # Button Colots Sheet
+    btnTextColor01 = '#ffffff'
+
+    btnColor01 = ''
+    btnColor02 = '#333333'
+
