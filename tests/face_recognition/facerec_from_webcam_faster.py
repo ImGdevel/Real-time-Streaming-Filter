16453a55a7ffcd115147fccf9b2a2f15de300bb8
TlesMes <56552726+TlesMes@users.noreply.github.com>
2024-04-01 21:32:03 +0900

    Update facerec_from_webcam_faster.py

--- tests/face_recognition/facerec_from_webcam_faster.py
+++ tests/face_recognition/facerec_from_webcam_faster.py
@@ -15,11 +15,11 @@ import numpy as np
 video_capture = cv2.VideoCapture(0)
 
 # Load a sample picture and learn how to recognize it.
-obama_image = face_recognition.load_image_file("obama.jpg")
+obama_image = face_recognition.load_image_file("./obama.jpg")
 obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
 
 # Load a second sample picture and learn how to recognize it.
-biden_image = face_recognition.load_image_file("biden.jpg")
+biden_image = face_recognition.load_image_file("./biden.jpg")
 biden_face_encoding = face_recognition.face_encodings(biden_image)[0]
 
 # Create arrays of known face encodings and their names
