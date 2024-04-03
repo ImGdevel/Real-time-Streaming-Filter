390ef1365d90bbf66441477f75d9d0b028ffa276
ImGdevel <imdlsrks.mc@gmail.com>
2024-04-03 21:03:48 +0900

    edit

--- ui/SampleLayout.py
+++ ui/SampleLayout.py
@@ -1,8 +1,9 @@
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QPushButton, QLabel, QGridLayout
+from PyQt5.QtWidgets import QWidget, QStackedWidget, QVBoxLayout, QPushButton, QLabel, QGridLayout
 from PyQt5.QtGui import QImage, QPixmap
 from PyQt5.QtCore import QTimer, Qt
 from .colors import Colors
-class HomePage(QWidget):
+
+class SampleLayout(QWidget):
     def __init__(self, parent = None):
         super().__init__(parent)
         self.initUI()
@@ -11,16 +12,17 @@ class HomePage(QWidget):
         # 레이아웃 설정
         layout = QGridLayout()
 
-        # 오른쪽 1/4 공간
+        # 오른쪽 1/4 공간 - 메뉴바
         right_widget = QWidget()
         right_widget.setStyleSheet(f'background-color: {Colors.baseColor01};')
-        right_widget.setMinimumSize(150, 700)
+        right_widget.setMinimumSize(100, 700)
         layout.addWidget(right_widget, 0, 0, 1, 1)
 
-        # 나머지 구간
+        # 나머지 구간 - 컨텐츠
         remaining_widget = QWidget()
-        remaining_widget.setStyleSheet(f'background-color: {Colors.btnColor02};')
-        remaining_widget.setMinimumSize(768, 700)
+        remaining_widget.setStyleSheet(f'background-color: {Colors.baseColor02};')
+        remaining_widget.setMinimumSize(500, 700)
         layout.addWidget(remaining_widget, 0, 1, 1, 1)
+
 
         self.setLayout(layout)
