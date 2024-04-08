e4aae106ac927ae5ba8b2babbe8bd44064a1d912
ImGdevel <imdlsrks.mc@gmail.com>
2024-04-08 22:39:59 +0900

    save

--- app/views/VideoView.py
+++ app/views/VideoView.py
@@ -1,8 +1,36 @@
-from PyQt5.QtWidgets import QWidget, QStackedWidget, QVBoxLayout, QHBoxLayout, QPushButton, QLabel, QGridLayout, QFileDialog
+import os
+import cv2
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel, QPushButton, QSlider, QFileDialog
 from PyQt5.QtGui import QImage, QPixmap
-from PyQt5.QtCore import QTimer, Qt, QUrl
-from utils import Colors
-from contollers.video_player_thread import VideoPlayerThread
+from PyQt5.QtCore import Qt, QThread, pyqtSignal
+
+class VideoInfo:
+    def __init__(self, video_path):
+        self.video_path = video_path
+        self.video_name = os.path.basename(video_path)
+        self.video_extension = os.path.splitext(video_path)[1][1:].lower()
+        self.video_size = os.path.getsize(video_path)
+
+class VideoPlayerThread(QThread):
+    video_frame = pyqtSignal(object)
+    current_frame = pyqtSignal(int)
+
+    def __init__(self, video_path):
+        super(VideoPlayerThread, self).__init__()
+        self.video_path = video_path
+        self.cap = cv2.VideoCapture(self.video_path)
+        self.video_frame_count = int(self.cap.get(cv2.CAP_PROP_FRAME_COUNT))
+
+    def run(self):
+        while self.cap.isOpened():
+            ret, frame = self.cap.read()
+            if ret:
+                current_frame_num = int(self.cap.get(cv2.CAP_PROP_POS_FRAMES))
+                self.video_frame.emit(frame)
+                self.current_frame.emit(current_frame_num)
+            else:
+                break
+        self.cap.release()
 
 class VideoView(QWidget):
     def __init__(self, parent=None):
@@ -10,45 +38,51 @@ class VideoView(QWidget):
         self.initUI()
 
     def initUI(self):
-        # 전체 레이아웃 설정
-        self.layout = QGridLayout()
+        self.layout = QVBoxLayout()
 
-        # 비디오 영역
-        self.right_widget = QWidget()
-        self.right_widget.setStyleSheet(f'background-color: {Colors.baseColor01};')
-        self.right_widget.setMinimumWidth(300)
-        self.layout.addWidget(self.right_widget, 0, 0, 1, 1)
-
-        self.video_layout = QVBoxLayout()
-        self.right_widget.setLayout(self.video_layout)
-
-       # 비디오 위젯 추가
+        # 비디오 위젯 추가
         self.video_widget = QLabel(self)
-        self.video_widget.setFixedSize(640, 480)  # 비디오 크기 설정
-        self.video_layout.addWidget(self.video_widget)
+        self.video_widget.setFixedSize(640, 480)
+        self.layout.addWidget(self.video_widget)
 
         # 파일 탐색기 버튼
         self.file_dialog_button = QPushButton("Choose Video File")
         self.file_dialog_button.clicked.connect(self.openFileDialog)
-        self.video_layout.addWidget(self.file_dialog_button)
+        self.layout.addWidget(self.file_dialog_button)
 
-        # video 바
-        self.video_bar = QWidget()
-        self.video_bar.setStyleSheet(f'background-color: {Colors.baseColor02};')
-        self.video_bar.setMaximumHeight(15)
-        self.video_layout.addWidget(self.video_bar)
-
-        # 메뉴 영역
-        self.remaining_widget = QWidget()
-        self.remaining_widget.setStyleSheet(f'background-color: {Colors.baseColor01};')
-        self.remaining_widget.setFixedWidth(150)
-        self.layout.addWidget(self.remaining_widget, 0, 1, 1, 1)
+        # 비디오 바 (슬라이더)
+        self.video_bar = QSlider(Qt.Horizontal)
+        self.video_bar.setEnabled(False)
+        self.video_bar.sliderMoved.connect(self.changeVideoPosition)
+        self.layout.addWidget(self.video_bar)
 
         self.setLayout(self.layout)
 
     def openFileDialog(self):
         options = QFileDialog.Options()
-        filePath, _ = QFileDialog.getOpenFileName(self, "Open Video File", "", "Video Files (*.mp4 *.avi);;All Files (*)", options=options)
+        filePath, _ = QFileDialog.getOpenFileName(self, "Open Video File", "", "Video Files (*.mp4 *.avi *.mkv *.flv);;All Files (*)", options=options)
         if filePath:
-            self.video_thread = VideoPlayerThread(filePath, self.video_widget)
+            self.video_info = VideoInfo(filePath)
+            self.video_thread = VideoPlayerThread(filePath)
+            self.video_thread.video_frame.connect(self.updateVideoFrame)
+            self.video_thread.current_frame.connect(self.updateVideoBar)
+            self.video_bar.setEnabled(True)
             self.video_thread.start()
+
+    def updateVideoFrame(self, frame):
+        rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        h, w, c = rgb_frame.shape
+        q_img = QImage(rgb_frame.data, w, h, w * c, QImage.Format_RGB888)
+        pixmap = QPixmap.fromImage(q_img)
+        self.video_widget.setPixmap(pixmap.scaled(self.video_widget.width(), self.video_widget.height()))
+
+    def updateVideoBar(self, current_frame_num):
+        total_frames = self.video_thread.video_frame_count
+        value = int((current_frame_num / total_frames) * 100)
+        self.video_bar.setValue(value)
+
+    def changeVideoPosition(self, value):
+        if hasattr(self, 'video_thread'):
+            total_frames = self.video_thread.video_frame_count
+            target_frame = int(value / 100 * total_frames)
+            self.video_thread.cap.set(cv2.CAP_PROP_POS_FRAMES, target_frame)
