5840991a40458859010c89f7681a1872e58fb038
ImGdevel <imdlsrks.mc@gmail.com>
2024-04-11 12:51:39 +0900

    Update RealStreamView.py

--- app/views/RealStreamView.py
+++ app/views/RealStreamView.py
@@ -29,40 +29,49 @@ class RealStreamView(QWidget):
     def setup_toolbar(self):
         '''툴바 설정 메서드'''
         self.toolbar = QWidget()  # 툴바 위젯
-        self.toolbar.setStyleSheet(f'background-color: {Colors.baseColor01}; border-radius: 5px;')  # 배경색 및 테두리 설정
+        self.toolbar.setStyleSheet(f'background-color: {Colors.baseColor01};')  # 배경색 및 테두리 설정
         self.toolbar.setFixedSize(300, 450)  # 크기 설정
         self.layout.addWidget(self.toolbar, 0, 0)
 
+        # 툴바 레이아웃 설정
+        toolbar_layout = QVBoxLayout()
+
         # 상단 핵심 버튼 설정
         self.setup_core_buttons()
+        toolbar_layout.addWidget(self.core_buttons_widget)
 
         # 중단 설정 (웹캠 선택, 비디오 배율)
         self.setup_video_options()
+        toolbar_layout.addWidget(self.video_options_widget)
 
         # 하단 필터 리스트
         self.setup_filter_list()
+        toolbar_layout.addWidget(self.filter_scroll_area)
+
+        toolbar_layout.setStretch(0, 1)  # 상단 버튼 레이아웃 높이 비율
+        toolbar_layout.setStretch(1, 3)  # 중단 비디오 옵션 설정 높이 비율
+        toolbar_layout.setStretch(2, 4)  # 하단 필터 리스트 높이 비율
+
+        self.toolbar.setLayout(toolbar_layout)
 
     def setup_core_buttons(self):
         '''상단 핵심 버튼 설정 메서드'''
-        font = QFont("Arial", 12)  # 폰트 설정
+        self.core_buttons_widget = QWidget()
 
         # 실시간 영상 재생/중지 버튼
-        self.play_pause_button = QPushButton("Play/Pause")
-        self.play_pause_button.setFont(font)
+        self.play_pause_button = QPushButton("Play\nPause")
+        self.play_pause_button.setFixedSize(70,70)
         self.play_pause_button.clicked.connect(self.toggle_webcam)
-        self.play_pause_button.setStyleSheet("QPushButton { border: 1px solid black; }")  # 윤곽선 추가
 
         # 일시정지 버튼
         self.stop_button = QPushButton("Stop")
-        self.stop_button.setFont(font)
+        self.stop_button.setFixedSize(70,70)
         self.stop_button.clicked.connect(self.stop_webcam)
-        self.stop_button.setStyleSheet("QPushButton { border: 1px solid black; }")  # 윤곽선 추가
 
         # 새 창 버튼
-        self.new_window_button = QPushButton("New Window")
-        self.new_window_button.setFont(font)
+        self.new_window_button = QPushButton("New\nWindow")
+        self.new_window_button.setFixedSize(70,70)
         self.new_window_button.clicked.connect(self.open_new_window)
-        self.new_window_button.setStyleSheet("QPushButton { border: 1px solid black; }")  # 윤곽선 추가
 
         # 상단 버튼 레이아웃 설정
         core_buttons_layout = QHBoxLayout()
@@ -71,21 +80,19 @@ class RealStreamView(QWidget):
         core_buttons_layout.addWidget(self.new_window_button)
         core_buttons_layout.setSpacing(10)  # 버튼 사이 간격 설정
 
-        self.toolbar.setLayout(core_buttons_layout)
+        self.core_buttons_widget.setLayout(core_buttons_layout)
 
     def setup_video_options(self):
         '''중단 비디오 옵션 설정 메서드'''
-        font = QFont("Arial", 12)  # 폰트 설정
+        self.video_options_widget = QWidget()
 
         # 웹캠 선택 콤보박스
         self.webcam_combo = QComboBox()
-        self.webcam_combo.setFont(font)
         self.webcam_combo.addItems(["0", "1"])  # 임시 웹캠 목록
         self.webcam_combo.currentIndexChanged.connect(self.change_webcam)
 
         # 비디오 배율 콤보박스
         self.aspect_ratio_combo = QComboBox()
-        self.aspect_ratio_combo.setFont(font)
         self.aspect_ratio_combo.addItems(["16:9", "3:4", "4:3", "9:16"])
         self.aspect_ratio_combo.currentIndexChanged.connect(self.change_aspect_ratio)
 
@@ -96,31 +103,23 @@ class RealStreamView(QWidget):
         video_options_layout.addWidget(QLabel("Aspect Ratio:"))
         video_options_layout.addWidget(self.aspect_ratio_combo)
 
-        video_options_widget = QWidget()
-        video_options_widget.setLayout(video_options_layout)
-
-        self.layout.addWidget(video_options_widget, 0, 1)
+        self.video_options_widget.setLayout(video_options_layout)
 
     def setup_filter_list(self):
         '''하단 필터 리스트 설정 메서드'''
-        font = QFont("Arial", 12)  # 폰트 설정
-
         # 필터 리스트
         self.filter_scroll_area = QScrollArea()
         self.filter_scroll_area.setWidgetResizable(True)
-        self.filter_scroll_area.setFixedSize(300, 150)
+        self.filter_scroll_area.setFixedSize(260, 150) 
 
         # 필터 버튼 생성
         self.flip_button = QPushButton("Flip Horizontal")
-        self.flip_button.setFont(font)
         self.flip_button.clicked.connect(self.apply_flip)
 
         self.mosaic_button = QPushButton("Toggle Mosaic")
-        self.mosaic_button.setFont(font)
         self.mosaic_button.clicked.connect(self.apply_mosaic)
 
         self.dummy_button = QPushButton("Dummy Button")
-        self.dummy_button.setFont(font)
         self.dummy_button.clicked.connect(self.dummy_function)
 
         # 필터 버튼 레이아웃 설정
@@ -135,26 +134,23 @@ class RealStreamView(QWidget):
 
         self.filter_scroll_area.setWidget(filter_widget)
 
-        self.layout.addWidget(self.filter_scroll_area, 1, 0, 1, 2)
-
     def setup_video_layer(self):
         '''비디오 레이어 설정 메서드'''
         self.video_widget = QLabel()  # 비디오 플레이어 레이블
-        self.video_widget.setStyleSheet(f'background-color: {Colors.baseColor01}; border-radius: 10px;')  # 배경색 및 테두리 설정
+        self.video_widget.setStyleSheet(f'background-color: {Colors.baseColor01};')  # 배경색 및 테두리 설정
         self.video_widget.setAlignment(Qt.AlignCenter)  # 정렬 설정
         self.layout.addWidget(self.video_widget, 0, 1)
 
     def setup_bottom_layer(self):
         '''하단 레이어 설정 메서드'''
         self.bottom_widget = QWidget()  # 하단 위젯
-        self.bottom_widget.setStyleSheet(f'background-color: {Colors.baseColor01}; border-radius: 10px;')  # 배경색 및 테두리 설정
+        self.bottom_widget.setStyleSheet(f'background-color: {Colors.baseColor01};')  # 배경색 및 테두리 설정
         self.bottom_layout = QVBoxLayout()  # 수직 레이아웃
         self.bottom_widget.setLayout(self.bottom_layout)
-
         self.layout.addWidget(self.bottom_widget, 1, 0, 1, 2)
 
-    
 
+    # method
     def toggle_webcam(self):
         '''웹캠 토글 메서드'''
         if self.play_pause_button.isChecked():
@@ -176,9 +172,9 @@ class RealStreamView(QWidget):
             self.timer.stop()
 
     
-    def open_new_window():
+    def open_new_window(self):
         '''새창 메서드'''
-        # 웹캠 변경 로직 추가
+        # 웹캠 새장 로직 추가
         pass
     
     
@@ -186,7 +182,6 @@ class RealStreamView(QWidget):
         '''웹캠 변경 메서드'''
         # 웹캠 변경 로직 추가
         pass
-
 
 
     def change_aspect_ratio(self, index):
