b6e15657127439f41d65661dd1401ed897141004
ImGdevel <imdlsrks.mc@gmail.com>
2024-04-15 17:33:24 +0900

    Update filter_setting_controller.py

--- app/controllers/filter_setting_controller.py
+++ app/controllers/filter_setting_controller.py
@@ -1,32 +1,8 @@
-from models import Filter
+from models import Filter, FilterManager
 
 class FilterSettingController:
     def __init__(self, filter_manager, filter_view):
-        self.filter_manager = filter_manager
-        self.filter_view = filter_view
-
-        # 필요한 초기화 작업
-        self.initialize()
-
-    def initialize(self):
-        """초기화 메서드"""
-        # FilterManager의 데이터를 FilterSettingView에 업데이트
-        self.update_filter_list_view()
-
-        # View에서 발생하는 이벤트에 대한 연결
-        self.connect_view_events()
-
-    def update_filter_list_view(self):
-        """FilterManager의 filter_list를 FilterSettingView에 업데이트"""
-        # FilterManager의 filter_list를 가져와 FilterSettingView의 filter_list_widget에 업데이트
-        filters = [filter_obj.name for filter_obj in self.filter_manager.filter_list]
-        self.filter_view.update_filter_list(filters)
-
-    def connect_view_events(self):
-        """View에서 발생하는 이벤트에 대한 연결"""
-        # FilterSettingView의 이벤트와 해당 메서드 연결
-        self.filter_view.add_filter_event.connect(self.add_filter)
-        self.filter_view.delete_filter_event.connect(self.delete_filter)
+        self.filter_manager = FilterManager()
 
     def add_filter(self, filter_name):
         """Filter 추가 메서드"""
@@ -44,3 +20,7 @@ class FilterSettingController:
 
         # FilterSettingView 업데이트
         self.update_filter_list_view()
+    
+    def update_filter(self, filter: Filter = None):
+        """FilterManager의 filter_list를 FilterSettingView에 업데이트"""
+        pass
