85ebd6b84c946f96879a5aa7fa73a24c8e64f78a
SH Woo <62339794+ImGdevel@users.noreply.github.com>
2024-04-27 17:35:10 +0900

    Update RealStreamView.py

--- app/views/RealStreamView.py
+++ app/views/RealStreamView.py
@@ -19,25 +19,24 @@ class RealStreamView(QWidget):
         '''GUI 초기화 메서드'''
         self.stream_main_layout = QGridLayout()  # 레이아웃 설정
 
-        self.setup_toolbar()
-        self.setup_video_layer()
-        self.setup_bottom_layer()
+        toolbar = self.setup_toolbar()
+        video_widget = self.setup_video_layer()
+        bottom_widget = self.setup_bottom_layer()
 
-        self.stream_main_layout.addWidget(self.toolbar, 0, 0)
-        self.stream_main_layout.addWidget(self.video_widget, 0, 1)
-        self.stream_main_layout.addWidget(self.bottom_widget, 1, 0, 1, 2)
+        self.stream_main_layout.addWidget(toolbar, 0, 0)
+        self.stream_main_layout.addWidget(video_widget, 0, 1)
+        self.stream_main_layout.addWidget(bottom_widget, 1, 0, 1, 2)
         
+        self.stream_main_layout.setRowStretch(0, 6)  # 상단 행 스트레칭 비율
+        self.stream_main_layout.setRowStretch(1, 3)  # 하단 행 스트레칭 비율
+
         self.setLayout(self.stream_main_layout)
 
-    def render(self):
-        """페이지 refesh"""
-        self.filter_list_widget.update_filter_list()
-        pass
 
     def setup_toolbar(self):
         '''툴바 설정 메서드'''
-        self.toolbar = ShadowWidget()  # 툴바 위젯
-        self.toolbar.setMaximumSize(300, 450)  # 크기 설정
+        toolbar = ShadowWidget()  # 툴바 위젯
+        toolbar.setMaximumWidth(250)  # 크기 설정
 
         # 툴바 레이아웃 설정
         toolbar_layout = QVBoxLayout()
@@ -54,16 +53,14 @@ class RealStreamView(QWidget):
         filter_list_layout = self.setup_filter_list()
         toolbar_layout.addWidget(filter_list_layout)
 
-        toolbar_layout.setStretch(0, 1)  # 상단 버튼 레이아웃 높이 비율
-        toolbar_layout.setStretch(1, 3)  # 중단 비디오 옵션 설정 높이 비율
-        toolbar_layout.setStretch(2, 4)  # 하단 필터 리스트 높이 비율
-
-        self.toolbar.setLayout(toolbar_layout)
+        toolbar.setLayout(toolbar_layout)
+        return toolbar
 
     def setup_core_buttons(self):
         '''상단 핵심 버튼 설정 메서드'''
         frame = QWidget()
         frame.setStyleSheet(Style.frame_style)
+        frame.setFixedHeight(70)
         
         core_buttons_layout = QHBoxLayout()
 
@@ -102,19 +99,20 @@ class RealStreamView(QWidget):
     def setup_video_options(self):
         '''중단 비디오 옵션 설정 메서드'''
         frame = QWidget()
+        frame.setFixedHeight(140)
         frame.setStyleSheet(Style.frame_style)
         
         video_options_layout = QVBoxLayout()
 
         # 웹캠 선택 콤보박스
-        webcam_combo_label = QLabel("Webcam:")
+        webcam_combo_label = QLabel("Webcam")
         self.webcam_combo = QComboBox()
         self.webcam_combo.setStyleSheet(f'background-color: {Colors.base_color_03}')
-        self.webcam_combo.addItems(["0", "1"])  # 임시 웹캠 목록
+        self.webcam_combo.addItems(["0", "1"])
         self.webcam_combo.currentIndexChanged.connect(self.change_webcam)
         
         # 비디오 배율 콤보박스
-        aspect_ratio_combo_label = QLabel("Aspect Ratio:")
+        aspect_ratio_combo_label = QLabel("Aspect Ratio")
         self.aspect_ratio_combo = QComboBox()
         self.aspect_ratio_combo.setStyleSheet(f'background-color: {Colors.base_color_03}')
         self.aspect_ratio_combo.addItems(["16:9", "3:4", "4:3", "9:16"])
@@ -150,14 +148,22 @@ class RealStreamView(QWidget):
 
     def setup_video_layer(self):
         '''비디오 레이어 설정 메서드'''
-        self.video_widget = QLabel()  # 비디오 플레이어 레이블
-        self.video_widget.setStyleSheet(f'background-color: {Colors.baseColor01};')  # 배경색 및 테두리 설정
-        self.video_widget.setAlignment(Qt.AlignCenter)  # 정렬 설정
+        frame = QFrame()
+        video_layout = QHBoxLayout()
+        
+        self.video_box = QLabel()  # 비디오 플레이어 레이블
+        self.video_box.setStyleSheet(f'background-color: {Colors.baseColor01};')  # 배경색 및 테두리 설정
+        self.video_box.setAlignment(Qt.AlignCenter)  # 정렬 설정
+        video_layout.addWidget(self.video_box)
+        frame.setLayout(video_layout)
+        
+        return frame
+        
 
     def setup_bottom_layer(self):
         '''하단 레이어 설정 메서드'''
-        self.bottom_widget = ShadowWidget()  # 하단 위젯
-        self.bottom_widget.setStyleSheet(f'background-color: {Colors.baseColor01};')  # 배경색 및 테두리 설정
+        bottom_widget = ShadowWidget()  # 하단 위젯
+        bottom_widget.setStyleSheet(f'background-color: {Colors.baseColor01};')  # 배경색 및 테두리 설정
         
         # 버튼 레이아웃 설정
         bottom_layout = QHBoxLayout()
@@ -197,7 +203,9 @@ class RealStreamView(QWidget):
         layout_temp.setStyleSheet(f'background-color: {Colors.base_color_Z};')
         bottom_layout.addWidget(layout_temp)
         
-        self.bottom_widget.setLayout(bottom_layout)
+        bottom_widget.setLayout(bottom_layout)
+        
+        return bottom_widget
 
 
     # method
@@ -234,28 +242,9 @@ class RealStreamView(QWidget):
         # 비디오 배율 변경 로직 추가
         pass
 
-    def apply_flip(self):
-        '''좌우반전 필터 적용 메서드'''
-        self.streaming_processor.flip_horizontal()
-        # 좌우반전 필터 적용 로직 추가
-        pass
-
     def set_filter_option(self, index):
         '''필터 옵션 선택'''
         self.streaming_processor.set_filter(index)
-        pass
-
-    def apply_mosaic(self):
-        '''모자이크 필터 적용 메서드'''
-        if self.mosaic_button.isChecked():
-            # 모자이크 활성화
-            self.streaming_processor.mosaic_active = True
-            self.mosaic_button.setText("Disable Mosaic")
-        else:
-            # 모자이크 비활성화
-            self.streaming_processor.mosaic_active = False
-            self.mosaic_button.setText("Toggle Mosaic")
-        # 모자이크 필터 적용 로직 추가
         pass
 
     def update_video(self, q_img=None):
@@ -263,11 +252,14 @@ class RealStreamView(QWidget):
         if q_img is None:
             return
         pixmap = QPixmap.fromImage(q_img)
-        self.video_widget.setPixmap(pixmap.scaled(self.video_widget.width(), self.video_widget.height(), Qt.KeepAspectRatio))
+        self.video_box.setPixmap(pixmap.scaled(self.video_box.width(), self.video_box.height(), Qt.KeepAspectRatio))
+
+    def render(self):
+        """페이지 refesh"""
+        self.filter_list_widget.update_filter_list()
+        pass
 
     def closeEvent(self, event):
         '''GUI 종료 이벤트 메서드'''
         self.streaming_processor.stop()
         self.timer.stop()
-
-    
