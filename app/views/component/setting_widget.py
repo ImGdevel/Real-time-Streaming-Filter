601ae90b86154d421231dff98da21dd271743c4a
SH Woo <62339794+ImGdevel@users.noreply.github.com>
2024-05-05 21:45:14 +0900

    error fix

--- app/views/component/setting_widget.py
+++ app/views/component/setting_widget.py
@@ -1,6 +1,6 @@
 from PySide6.QtWidgets import QWidget, QStackedWidget, QVBoxLayout, QPushButton, QLabel, QGridLayout, QListWidget
 from PySide6.QtGui import QImage, QPixmap
-from utils import Colors,Style
+from utils import Colors, Style
 
 class SettingWidget(QWidget):
     def __init__(self, parent = None):
@@ -9,7 +9,7 @@ class SettingWidget(QWidget):
         self.initUI()
 
     def initUI(self):
-        self.setStyleSheet(Style.base_style01)
+        self.setStyleSheet(Style.frame_style)
         self.layout = QVBoxLayout()
         self.layout.setSpacing(3)
         self.setLayout(self.layout)
