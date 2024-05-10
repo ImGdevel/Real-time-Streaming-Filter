8bb9b3d5ea77886e7bdb14ebe328fd05a61fafae
SH Woo <62339794+ImGdevel@users.noreply.github.com>
2024-05-10 12:11:38 +0900

    upload

--- tests/ImageCutter/test.py
+++ tests/ImageCutter/test.py
@@ -48,35 +48,35 @@ class ImageEditor(QMainWindow):
         self.reset_button.clicked.connect(self.reset_image)
         self.layout.addWidget(self.reset_button)
 
-        # 슬라이더 추가
-        self.slider_x = QSlider(Qt.Horizontal)
-        self.slider_x.setMinimum(-200)
-        self.slider_x.setMaximum(200)
-        self.slider_x.setValue(0)
-        self.slider_x.setTickInterval(10)
-        self.slider_x.setTickPosition(QSlider.TicksBelow)
-        self.slider_x.valueChanged.connect(self.update_image_position)
-        self.layout.addWidget(self.slider_x)
+        # # 슬라이더 추가
+        # self.slider_x = QSlider(Qt.Horizontal)
+        # self.slider_x.setMinimum(-200)
+        # self.slider_x.setMaximum(200)
+        # self.slider_x.setValue(0)
+        # self.slider_x.setTickInterval(10)
+        # self.slider_x.setTickPosition(QSlider.TicksBelow)
+        # self.slider_x.valueChanged.connect(self.update_image_position)
+        # self.layout.addWidget(self.slider_x)
 
-        self.slider_y = QSlider(Qt.Horizontal)
-        self.slider_y.setMinimum(-200)
-        self.slider_y.setMaximum(200)
-        self.slider_y.setValue(0)
-        self.slider_y.setTickInterval(10)
-        self.slider_y.setTickPosition(QSlider.TicksBelow)
-        self.slider_y.valueChanged.connect(self.update_image_position)
-        self.layout.addWidget(self.slider_y)
+        # self.slider_y = QSlider(Qt.Horizontal)
+        # self.slider_y.setMinimum(-200)
+        # self.slider_y.setMaximum(200)
+        # self.slider_y.setValue(0)
+        # self.slider_y.setTickInterval(10)
+        # self.slider_y.setTickPosition(QSlider.TicksBelow)
+        # self.slider_y.valueChanged.connect(self.update_image_position)
+        # self.layout.addWidget(self.slider_y)
 
-        self.slider_scale = QSlider(Qt.Horizontal)
-        self.slider_scale.setMinimum(1)
-        self.slider_scale.setMaximum(3)
-        self.slider_scale.setValue(1)
-        self.slider_scale.setTickInterval(0.1)
-        self.slider_scale.setTickPosition(QSlider.TicksBelow)
-        self.slider_scale.valueChanged.connect(self.update_image_scale)
-        self.layout.addWidget(self.slider_scale)
+        # self.slider_scale = QSlider(Qt.Horizontal)
+        # self.slider_scale.setMinimum(1)
+        # self.slider_scale.setMaximum(3)
+        # self.slider_scale.setValue(1)
+        # self.slider_scale.setTickInterval(0.1)
+        # self.slider_scale.setTickPosition(QSlider.TicksBelow)
+        # self.slider_scale.valueChanged.connect(self.update_image_scale)
+        # self.layout.addWidget(self.slider_scale)
 
-        self.origin_image = None  # 원본 이미지 저장용 변수
+        self.edit_image = None  # 원본 이미지 저장용 변수
         self.edit_image = None  # 편집된 이미지 저장용 변수
         self.offset = QPoint()  # 이미지 이동을 위한 마우스 클릭 시 좌표 저장
 
@@ -91,16 +91,13 @@ class ImageEditor(QMainWindow):
         if filename:
             image = QPixmap(filename)
             image_scaled = image.scaled(400, 400, Qt.KeepAspectRatio, Qt.SmoothTransformation)
-            self.image_label.setPixmap(image_scaled)
             self.origin_image = image
-            self.edit_image = image  # 편집된 이미지 초기화
+            self.edit_image = image_scaled
             self.image_posX = 0
             self.image_posY = 0
-            self.image_scale = 1.0
-            # 이미지를 불러올 때 슬라이더를 초기화합니다.
-            self.slider_x.setValue(0)
-            self.slider_y.setValue(0)
-            self.slider_scale.setValue(1)
+            self.image_scale = 0.4
+            self.show_image()
+
 
     def save_image(self):
         if self.edit_image:
@@ -109,11 +106,11 @@ class ImageEditor(QMainWindow):
                 self.edit_image.save(file_path)
 
     def crop_image(self):
-        if self.origin_image:
+        if self.edit_image:
             # 이미지의 크기를 계산합니다.
-            image_width = int(self.origin_image.width() * self.image_scale)
-            image_height = int(self.origin_image.height() * self.image_scale)
-            
+            image_width = int(self.edit_image.width() * self.image_scale)
+            image_height = int(self.edit_image.height() * self.image_scale)
+
             # 잘라낼 영역의 크기를 계산합니다.
             crop_width = min(self.image_frame.width(), image_width - self.image_posX)
             crop_height = min(self.image_frame.height(), image_height - self.image_posY)
@@ -125,27 +122,45 @@ class ImageEditor(QMainWindow):
             self.image_label.setPixmap(cropped_image)
 
             # 잘라낸 이미지를 edit_image에 저장하여 다음 작업에 사용합니다.
-            self.edit_image = cropped_image
+            self.cut_image = cropped_image
+            
+            print("수정된 이미지", self.edit_image)
+            
+            
+    def crop_and_fill(image, target_size = 400):
+        # 이미지를 원하는 크기로 잘라냅니다.
+        cropped_image = image.copy(0, 0, target_size.width(), target_size.height())
+        
+        # 새로운 크기로 이미지를 만듭니다.
+        new_image = QPixmap(target_size)
+        new_image.fill(Qt.transparent)  # 투명한 알파값으로 초기화합니다.
+        
+        # 잘라낸 이미지를 새로운 이미지의 원하는 위치에 붙여넣습니다.
+        painter = QPainter(new_image)
+        painter.drawPixmap((target_size.width() - cropped_image.width()) // 2, (target_size.height() - cropped_image.height()) // 2, cropped_image)
+        painter.end()
+        
+        return new_image
                 
     def reset_image(self):
-        if self.origin_image:
+        if self.edit_image:
             # 원본 이미지로 되돌립니다.
             self.edit_image = self.origin_image
-            self.image_label.setPixmap(self.edit_image)
+            self.show_image()
 
     def mousePressEvent(self, event):
         if event.buttons() == Qt.LeftButton:
             self.offset = event.globalPosition() - self.image_label.pos()
 
     def mouseMoveEvent(self, event):
-        if event.buttons() == Qt.LeftButton and self.origin_image:
+        if event.buttons() == Qt.LeftButton and self.edit_image:
             self.new_pos = event.globalPosition() - self.offset
             self.image_posX = self.new_pos.x()
             self.image_posY = self.new_pos.y()
             self.update_image_position()
 
     def wheelEvent(self, event):
-        if self.origin_image:
+        if self.edit_image:
             delta = event.angleDelta().y() / 120
             if delta > 0:
                 # 휠을 위로 굴리면 이미지 확대
@@ -157,18 +172,20 @@ class ImageEditor(QMainWindow):
 
     def update_image_scale(self):
         # 슬라이더 값을 기반으로 이미지를 확대 또는 축소합니다.
-        self.image_scale = self.slider_scale.value()
-        transform = QTransform().scale(self.image_scale, self.image_scale)
+        transform = QTransform().translate(self.image_posX, self.image_posY).scale(self.image_scale, self.image_scale).rotate(180)
         transformed_pixmap = self.origin_image.transformed(transform, Qt.SmoothTransformation)
-        self.image_label.setPixmap(transformed_pixmap)
-        # 편집된 이미지도 함께 업데이트합니다.
+        
+        print(transformed_pixmap)
         self.edit_image = transformed_pixmap
+        self.image_label.setPixmap(self.edit_image)
+        self.image_label.move(self.image_posX, self.image_posY)
 
     def update_image_position(self):
         # 슬라이더 값을 기반으로 이미지의 위치를 업데이트합니다.
-        self.image_posX = self.slider_x.value()
-        self.image_posY = self.slider_y.value()
         self.image_label.move(self.image_posX, self.image_posY)
+        
+    def show_image(self):
+        self.image_label.setPixmap(self.edit_image)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
