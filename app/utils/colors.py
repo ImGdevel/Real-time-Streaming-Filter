16addbce826265ec3dd70313ebca2182f1662be5
SH Woo <62339794+ImGdevel@users.noreply.github.com>
2024-05-15 17:11:55 +0900

    CHANGE BLUE

--- app/utils/colors.py
+++ app/utils/colors.py
@@ -1,29 +1,8 @@
 # colors.py
 
 class Colors:
-    #Title Bar Colors
-    titleBarBackgroundColor = '#1a1a1a'
-    cancelBtnColor = '#c33d3d'
-
-    #TextColorSheet
-    textColor01 = '#ffffff'
     
-    baseColor00 = '#000000'
-    baseColor01 = 'rgb(23, 26, 30)'
-    
-    baseColor02 = '#2a2a2a'
-    baseColor04 = 'rgb(33, 37, 43)'
-    # Button Colots Sheet
-    btnTextColor01 = '#ffffff'
-
-    buttonColor_01_base = '#212121'
-    buttonColor_01_hover = '#111111'
-    buttonColor_01_select = '#C583F4'
-    
-    buttonColor_02_base = 'rgb(44, 49, 60)'
-
-    # 테두리색
-    borderColor01 = '#111111'
+        
     
     base_color_01 = 'rgb(23, 26, 30)'
     base_color_02 = 'rgb(27, 29, 35)'
@@ -35,9 +14,30 @@ class Colors:
     base_color_08 = 'rgb(64, 71, 88)'
     base_color_09 = 'rgb(91, 101, 124)'
     
-    base_color_X = 'rgb(189, 147, 249)'
-    base_color_Y = 'rgb(255, 121, 198)'
-    base_color_Z = 'rgb(221, 221, 221)'
-    
+
+    identity_color_01 = 'rgb(100, 181, 246)'
+    identity_color_02 = 'rgb(66, 165, 245)'
+    identity_color_03 = 'rgb(33, 150, 243)'
+    identity_color_04 = 'rgb(30, 136, 229)'
+    identity_color_05 = 'rgb(25, 118, 210)'
+    identity_color_06 = 'rgb(21, 101, 192)'
+    identity_color_07 = 'rgb(13, 71, 161)'
+
     base_color_F = 'rgba(225, 225, 225, 0)'
+    
+    
+    #TextColorSheet
+    textColor01 = '#ffffff'
+    
+    baseColor01 = 'rgb(23, 26, 30)'
+
+    baseColor04 = 'rgb(33, 37, 43)'
+    # Button Colots Sheet
+    btnTextColor01 = '#ffffff'
+    
+    buttonColor_02_base = 'rgb(44, 49, 60)'
+    # 테두리색
+    borderColor01 = '#111111'
+    
+
 
