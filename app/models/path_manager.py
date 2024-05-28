d9f75e19e9313d88713cd8ddaa848ca5c7cb786a
ImGdevel <imdlsrks.mc@gmail.com>
2024-05-28 18:02:18 +0900

    name change

--- app/models/path_manager.py
+++ app/models/path_manager.py
@@ -84,20 +84,20 @@ class PathManager:
         documents_folder = find_documents_folder()
         if documents_folder:
             # Documents 폴더 내에 임시 다운로드 폴더 생성
-            download_folder = os.path.join(documents_folder, 'CAMA')
+            download_folder = os.path.join(documents_folder, 'OBLIND')
             # TempDownloads 폴더가 없으면 생성
             if not os.path.exists(download_folder):
                 os.makedirs(download_folder)
-                print("CAMA 폴더를 생성했습니다.")
+                print("OBLIND 폴더를 생성했습니다.")
             if not os.path.exists(os.path.join(download_folder, "Images")):
                 os.makedirs(os.path.join(download_folder, "Images"))
-                print("CAMA/Images 폴더를 생성했습니다.")
+                print("OBLIND/Images 폴더를 생성했습니다.")
             if not os.path.exists(os.path.join(download_folder, "Videos")):
                 os.makedirs(os.path.join(download_folder, "Videos"))
-                print("CAMA/Videos 폴더를 생성했습니다.")
+                print("OBLIND/Videos 폴더를 생성했습니다.")
             if not os.path.exists(os.path.join(download_folder, "Recodes")):
                 os.makedirs(os.path.join(download_folder, "Recodes"))
-                print("CAMA/Recodes 폴더를 생성했습니다.")
+                print("OBLIND/Recodes 폴더를 생성했습니다.")
             return download_folder
         else:
             print("Documents 폴더를 찾을 수 없습니다.")
